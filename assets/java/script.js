99e61fd142dc1af2db3eab5a68740995e3d3c1ca
jacobxthomas <jacobthomas0801@gmail.com>
2021-05-11 17:51:44 +0800

    added UV and empty

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,25 +1,37 @@
-var searchButton = document.querySelector(".searchButton");
-var apiKey = "1de82ef4fc7c78999ed3baa696fd52ce"
+const searchButton = document.querySelector(".searchButton");
+const apiKey = "1de82ef4fc7c78999ed3baa696fd52ce"
 
 searchButton.addEventListener("click", userSearch);
 
 function userSearch() {
 
-    var searchInput = document.querySelector(".searchInput").value
-    var urlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&Appid=" + apiKey + "&units=imperial";
+    const searchInput = document.querySelector(".searchInput").value
+    const urlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&Appid=" + apiKey + "&units=imperial";
 
     $.ajax({
         url: urlCurrent,
         method: "GET"
     }).then(function (response) {
 
-        var currentData = $(".currentData")
-        var currentTime = new Date(response.dt * 1000);
+        const currentData = $(".currentData");
+        currentData.empty();
+        const currentTime = new Date(response.dt * 1000);
 
         currentData.append(response.name + " Date: " + currentTime.toLocaleDateString("en-AU"));
         currentData.append(`<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png">`)
         currentData.append("<p>" + "Temperature: " + response.main.temp + "</p>");
         currentData.append("<p>" + "Humidity: " + response.main.humidity + "</p>");
         currentData.append("<p>" + "Wind Speed: " + response.wind.speed + "</p>");
+
+        const urlUV = `https://api.openweathermap.org/data/2.5/uvi?appid=b8ecb570e32c2e5042581abd004b71bb&lat=${response.coord.lat}&lon=${response.coord.lon}`;
+
+        $.ajax({
+            url: urlUV,
+            method: "GET"
+        }).then(function (response) {
+
+        currentData.append("<p>" + "UV Index: " + response.value + "</p>")
+            
+        });
     })
 }
