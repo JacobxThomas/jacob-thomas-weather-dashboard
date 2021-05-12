20d7c7b5306ed856f3865981ce81341f29cae389
jacobxthomas <jacobthomas0801@gmail.com>
2021-05-12 11:37:21 +0800

    added five day forcast and search list and some img styling

--- assets/java/script.js
+++ assets/java/script.js
@@ -7,17 +7,22 @@ function userSearch() {
 
     const searchInput = document.querySelector(".searchInput").value
     const urlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&Appid=" + apiKey + "&units=metric";
+    var urlFiveDayForcast = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInput + "&Appid=" + apiKey + "&units=metric";
 
     $.ajax({
         url: urlCurrent,
         method: "GET"
     }).then(function (response) {
 
+        const citySearch = $(".list").addClass("list-item");
+        citySearch.append("<button>" + response.name + "</button>");
+
+
         const currentData = $(".currentData");
         currentData.empty();
         const currentTime = new Date(response.dt * 1000);
 
-        currentData.append("<p>" + response.name + " Date: " + currentTime.toLocaleDateString("en-AU")+ "</p>");
+        currentData.append("<p>" + response.name + " Date: " + currentTime.toLocaleDateString("en-AU") + "</p>");
         currentData.append(`<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png">`)
         currentData.append("<p>" + "Temperature: " + response.main.temp + " °C" + "</p>");
         currentData.append("<p>" + "Humidity: " + response.main.humidity + " %" + "</p>");
@@ -30,8 +35,24 @@ function userSearch() {
             method: "GET"
         }).then(function (response) {
 
-        currentData.append("<p>" + "UV Index: " + response.value + " nm" +  "</p>")
-            
+            currentData.append("<p>" + "UV Index: " + response.value + " nm" + "</p>")
+
+        });
+
+        $.ajax({
+            url: urlFiveDayForcast,
+            method: "GET"
+        }).then(function (response) {
+            var day = [0, 8, 16, 24, 32];
+            var fiveDayData = $(".fiveDayData");
+
+            fiveDayData.empty();
+
+            day.forEach(function (i) {
+                var FiveDayTimeUTC1 = new Date(response.list[i].dt * 1000);
+                FiveDayTimeUTC1 = FiveDayTimeUTC1.toLocaleDateString("en-AU");
+                fiveDayData.append("<div>" + "<p>" + "Date: " + FiveDayTimeUTC1 + "</p>" + `<img src="https://openweathermap.org/img/wn/${response.list[i].weather[0].icon}@2x.png">` + "<p>" + "Temperature: " + response.list[i].main.temp + " °C" + "</p>" + "<p>" + "Humidity: " + response.list[i].main.humidity + "%" + "</p>" + "</div>")
+            })
         });
     })
 }
